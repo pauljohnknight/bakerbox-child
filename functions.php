d8955f517aaffd5418a4b76485f14a6776f8e4be
Paul Knight <pauljohnknight@gmail.com>
2017-02-25 01:56:28 +0000

    tweaked childtheme code in functions.php

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ function my_theme_enqueue_styles() {
   wp_enqueue_style( 'child-style',
     get_stylesheet_directory_uri() . '/style.css',
     array( $parent_style ),
-    wp_get_theme()->get('')  //must match childtheme version if uses
+    wp_get_theme()->get('Version')
   );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
